4eb687097b7a4dab201c208b579705338bba7853
Sylester-Oputa <soputa42@gmail.com>
2025-12-04 09:16:22 +0100

    Remove dist folder from git tracking - Railway will build fresh

--- backend/dist/data.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-exports.generateId = generateId;
-const uuid_1 = require("uuid");
-exports.users = [];
-function generateId() {
-    return (0, uuid_1.v4)();
-}
